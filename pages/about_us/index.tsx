c8d1d805bf82db2c9749976821bc4e8ed475b810
ripinsurreality <ripinsurreality@gmail.com>
2021-07-13 18:16:55 +0300

    add about us animation

--- pages/about_us/index.tsx
+++ pages/about_us/index.tsx
@@ -3,6 +3,9 @@ import styles from "./aboutus.module.sass"
 import Member from "@modules/member/Member"
 import Image from "next/image"
 import { v4 as uuid } from "uuid"
+import { useEffect, useRef, useState } from "react"
+import SimpleBar from "simplebar-react"
+import "simplebar/src/simplebar.css"
 
 interface Props {}
 
@@ -48,28 +51,65 @@ const AboutUs: React.FC<Props> = ({}) => {
       post: "Business process analyst",
       img: "member1.png",
     },
-    {
-      name: "Sara Adams",
-      post: "Business process analyst",
-      img: "member2.png",
-    },
-    {
-      name: "zlata orlova",
-      post: "Business process analyst",
-      img: "member2.png",
-    },
   ]
   const teamItems = team.map(({ name, post }, i) => (
     <li className={styles.team__member} key={uuid()}>
       <Member name={name} post={post} />
     </li>
   ))
+  const [isTop, setIsTop] = useState(true)
+  const team$ = useRef<HTMLDivElement>(null)
+
+  const [isSmall, setIsSmall] = useState(false)
+
+  const isWheelDown = (delta: number) => {
+    if (delta > 0) return true
+    return false
+  }
+
+  useEffect(() => {
+    if (isSmall) return
+    const content = document.querySelectorAll(".simplebar-content")[1]
+    const moeseus = (e: WheelEvent) => {
+      if (isWheelDown(e.deltaY) && isTop) setIsTop(false)
+      if (
+        !isWheelDown(e.deltaY) &&
+        !isTop &&
+        team$.current !== null &&
+        content.getBoundingClientRect().top! >= 0
+      )
+        setIsTop(true)
+    }
+    document.addEventListener("wheel", moeseus, true)
+    return () => {
+      document.removeEventListener("wheel", moeseus, true)
+    }
+  }, [isTop, isSmall])
+  useEffect(() => {
+    const watch = (e: MediaQueryListEvent) => {
+      setIsSmall(e.matches)
+    }
+    const media = window.matchMedia("(max-width: 990px)")
+    console.log(media)
+    setIsSmall(media.matches)
+    media.addEventListener("change", watch)
+    return () => {
+      media.removeEventListener("change", watch)
+    }
+  }, [])
   return (
     <>
-      <Nav />
       <div className={styles.content}>
+        <Nav
+          style={
+            !isSmall
+              ? { position: "absolute", top: "0" }
+              : { position: "sticky" }
+          }
+        />
+
         <div className={styles.about}>
-          <div className={styles.about__left}>
+          <div className={styles.about__left} data-hidden={!isTop}>
             <div className={styles.about__logo}>
               <Image
                 layout={"responsive"}
@@ -80,7 +120,7 @@ const AboutUs: React.FC<Props> = ({}) => {
               />
             </div>
           </div>
-          <div className={styles.about__right}>
+          <div className={styles.about__right} data-hidden={!isTop}>
             <div className={styles.about__title}>Bravo Consulting</div>
             <p>
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum
@@ -98,10 +138,22 @@ const AboutUs: React.FC<Props> = ({}) => {
             </p>
           </div>
         </div>
-        <div className={styles.team}>
-          <div className={styles.team__title}>Our Team</div>
-          <ul className={styles.team__members}>{teamItems}</ul>
-        </div>
+
+        {isSmall ? (
+          <div className={styles.team} ref={team$}>
+            <div className={styles.team__title}>Our Team</div>
+            <ul className={styles.team__members}>{teamItems}</ul>
+          </div>
+        ) : (
+          <div className={styles.teamWrapper} data-hidden={isTop}>
+            <SimpleBar>
+              <div className={styles.team} ref={team$}>
+                <div className={styles.team__title}>Our Team</div>
+                <ul className={styles.team__members}>{teamItems}</ul>
+              </div>
+            </SimpleBar>
+          </div>
+        )}
       </div>
     </>
   )
